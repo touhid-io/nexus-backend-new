d3b94ade12a425b0f2e7d9cc63a6fe26c8c1fa21
Touhidul Islam <213492071+touhid-io@users.noreply.github.com>
2025-09-18 21:00:12 +0600

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -3,14 +3,14 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', '4ju47489ehdh83ur9w4u7b49fu84jdu94hu4'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', '8u8rur8urhfeur93894urklew94u94uth49r0eh94u39r09090u8h4ht849i33959h8u'),
+    salt: env('API_TOKEN_SALT', '2c39ae9805be40786bcf86e223fcdbf35079660df1f0a293de089122eddf9361'),
     secrets: {
       encryptionKey: env('5he8n388su9us9u348ye83y93uie03jhs83y39u383ueh8w44hi39598'),
     },
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', '9iri49ir033rii4t95hjwr93993jj49fni4999r94r9ut4'),
+      salt: env('TRANSFER_TOKEN_SALT', '1169cbff33835b0035bbf749b1864dcc80b3963a8f67e50c7e6f92a0fd896282'),
     },
   },
 });
